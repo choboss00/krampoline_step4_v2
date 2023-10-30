9461ceab261b2f02ec8d725cc29fbdb0a9bac2ce
sda587744 <sda587744@daum.net>
2023-10-30 04:29:48 +0000

    fix : init.sql 파일 수정
    Signed-off-by: sda587744 <sda587744@daum.net>

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -6,21 +6,139 @@ FLUSH PRIVILEGES;
 
 USE `krampoline`;
 
-DROP TABLE IF EXISTS `interests`;
+SET foreign_key_checks = 0;    # 외래키 체크 설정 해제
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS interests;
+DROP TABLE IF EXISTS mentor_posts;
+DROP TABLE IF EXISTS connected_users;
+DROP TABLE IF EXISTS not_connected_register_users;
+DROP TABLE IF EXISTS user_interests;
+DROP TABLE IF EXISTS videos;
+DROP TABLE IF EXISTS subtitles;
+DROP TABLE IF EXISTS video_interests;
+SET foreign_key_checks = 1;    # 외래키 체크 설정
+
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       created_at TIMESTAMP NOT NULL,
+                       deleted_at TIMESTAMP,
+                       is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+                       updated_at TIMESTAMP,
+                       age INT NOT NULL,
+                       country VARCHAR(255) NOT NULL,
+                       email VARCHAR(100) NOT NULL UNIQUE,
+                       first_name VARCHAR(255) NOT NULL,
+                       introduction VARCHAR(255),
+                       last_name VARCHAR(255) NOT NULL,
+                       PASSWORD VARCHAR(256) NOT NULL,
+                       phone VARCHAR(255) NOT NULL,
+                       profile_image VARCHAR(255),
+                       role VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE interests (
-   id int auto_increment,
-   created_at timestamp not null,
-   deleted_at timestamp,
-   is_deleted boolean default false not null,
-   updated_at timestamp,
-   category varchar(255),
-   primary key (id)
+                           id INT AUTO_INCREMENT PRIMARY KEY,
+                           created_at TIMESTAMP NOT NULL,
+                           deleted_at TIMESTAMP,
+                           is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+                           updated_at TIMESTAMP,
+                           category VARCHAR(255)
+);
+
+CREATE TABLE videos (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        created_at TIMESTAMP NOT NULL,
+                        deleted_at TIMESTAMP,
+                        is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+                        updated_at TIMESTAMP,
+                        video_end_time VARCHAR(255),
+                        video_info VARCHAR(255),
+                        video_start_time VARCHAR(255),
+                        video_url VARCHAR(255)
+);
+
+CREATE TABLE mentor_posts (
+                              id INT AUTO_INCREMENT PRIMARY KEY,
+                              created_at TIMESTAMP NOT NULL,
+                              deleted_at TIMESTAMP,
+                              is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+                              updated_at TIMESTAMP,
+                              content VARCHAR(300),
+                              state VARCHAR(255) NOT NULL,
+                              title VARCHAR(255) NOT NULL,
+                              writer_id INT,
+                              FOREIGN KEY (writer_id) REFERENCES users(id)
+);
+
+CREATE TABLE connected_users (
+                                 id INT AUTO_INCREMENT PRIMARY KEY,
+                                 created_at TIMESTAMP NOT NULL,
+                                 deleted_at TIMESTAMP,
+                                 is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+                                 updated_at TIMESTAMP,
+                                 mentee_user_id INT,
+                                 mentor_post_id INT,
+                                 FOREIGN KEY (mentee_user_id) REFERENCES users(id),
+                                 FOREIGN KEY (mentor_post_id) REFERENCES mentor_posts(id)
+);
+
+CREATE TABLE not_connected_register_users (
+                                              id INT AUTO_INCREMENT PRIMARY KEY,
+                                              created_at TIMESTAMP NOT NULL,
+                                              deleted_at TIMESTAMP,
+                                              is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+                                              updated_at TIMESTAMP,
+                                              state VARCHAR(255) NOT NULL,
+                                              mentee_user_id INT,
+                                              mentor_post_id INT,
+                                              FOREIGN KEY (mentee_user_id) REFERENCES users(id),
+                                              FOREIGN KEY (mentor_post_id) REFERENCES mentor_posts(id)
+);
+
+CREATE TABLE user_interests (
+                                id INT AUTO_INCREMENT PRIMARY KEY,
+                                created_at TIMESTAMP NOT NULL,
+                                deleted_at TIMESTAMP,
+                                is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+                                updated_at TIMESTAMP,
+                                interest_id INT,
+                                user_id INT,
+                                FOREIGN KEY (interest_id) REFERENCES interests(id),
+                                FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE video_interests (
+                                 id INT AUTO_INCREMENT PRIMARY KEY,
+                                 created_at TIMESTAMP NOT NULL,
+                                 deleted_at TIMESTAMP,
+                                 is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+                                 updated_at TIMESTAMP,
+                                 interest_id INT,
+                                 video_id INT,
+                                 FOREIGN KEY (interest_id) REFERENCES interests(id),
+                                 FOREIGN KEY (video_id) REFERENCES videos(id)
+);
+
+CREATE TABLE subtitles (
+                           id INT AUTO_INCREMENT PRIMARY KEY,
+                           created_at TIMESTAMP NOT NULL,
+                           deleted_at TIMESTAMP,
+                           is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+                           updated_at TIMESTAMP,
+                           eng_end_time VARCHAR(255),
+                           eng_start_time VARCHAR(255),
+                           eng_subtitle_content VARCHAR(255),
+                           kor_end_time VARCHAR(255),
+                           kor_start_time VARCHAR(255),
+                           kor_subtitle_content VARCHAR(255),
+                           video_id INT,
+                           FOREIGN KEY (video_id) REFERENCES videos(id)
 );
 
 INSERT INTO `interests` (created_at, category) VALUES
-  (NOW(), 'IDOL'),
-  (NOW(), 'Game'),
-  (NOW(), 'K-POP'),
-  (NOW(), 'Sports'),
-  (NOW(), 'Movie'),
-  (NOW(), 'Drama');
+                                                   (NOW(), 'IDOL'),
+                                                   (NOW(), 'Game'),
+                                                   (NOW(), 'K-POP'),
+                                                   (NOW(), 'Sports'),
+                                                   (NOW(), 'Movie'),
+                                                   (NOW(), 'Drama');
